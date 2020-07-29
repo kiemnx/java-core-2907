943620f5ef700315823e8af9cf1beafae6a4acb2
kiemnx <kiemnguyenxuan@gmail.com>
2020-07-29 21:21:50 +0700

    first commit

---
+++ src/vn/plusplus/javacore/HelloWorld.java
@@ -0,0 +1,7 @@
+package vn.plusplus.javacore;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("This is first program Java");
+    }
+}
---
+++ src/vn/plusplus/javacore/lesson1/Activity11.java
@@ -0,0 +1,7 @@
+package vn.plusplus.javacore.lesson1;
+
+public class Activity11 {
+    public static void main(String[] args) {
+        System.out.println("This is Activity 11");
+    }
+}
