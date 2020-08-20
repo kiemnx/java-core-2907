40c0090d2246bd850a312ea5f094aab71a2dfdfe
kiemnx <kiemnguyenxuan@gmail.com>
2020-08-20 19:59:21 +0700

    Update kiemnx branch

--- src/vn/plusplus/javacore/Application.java
+++ src/vn/plusplus/javacore/Application.java
@@ -5,5 +5,6 @@ import vn.plusplus.javacore.lesson6.Student;
 public class Application {
     public static void main(String[] args) {
 //        Student stA = new Student("", )
+        System.out.println("Day la nhanh kiemnx");
     }
 }
